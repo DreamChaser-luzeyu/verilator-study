ab59de4e06ec663d591f2500d4ca70c55b145a97
DreamChaser <yydluzeyu@gmail.com>
2023-07-27 02:56:10 +0800

    更新nvboard_minimum示例工程

--- nvboard_demo_minimal_proj/vsrc/top.v
+++ nvboard_demo_minimal_proj/vsrc/top.v
@@ -2,16 +2,8 @@ module top (
     input clk,
     input rst,
     input [7:0] sw,
-    input ps2_clk,
-    input ps2_data,
     output [15:0] ledr,
-    output VGA_CLK,
-    output VGA_HSYNC,
-    output VGA_VSYNC,
-    output VGA_BLANK_N,
-    output [7:0] VGA_R,
-    output [7:0] VGA_G,
-    output [7:0] VGA_B,
+    
     output [7:0] seg0,
     output [7:0] seg1,
     output [7:0] seg2,
@@ -29,32 +21,32 @@ led led1(
     .ledr(ledr)
 );
 
-assign VGA_CLK = clk;
+// assign VGA_CLK = clk;
 
-wire [9:0] h_addr;
-wire [9:0] v_addr;
-wire [23:0] vga_data;
+// wire [9:0] h_addr;
+// wire [9:0] v_addr;
+// wire [23:0] vga_data;
 
-vga_ctrl my_vga_ctrl(
-    .pclk(clk),
-    .reset(rst),
-    .vga_data(vga_data),
-    .h_addr(h_addr),
-    .v_addr(v_addr),
-    .hsync(VGA_HSYNC),
-    .vsync(VGA_VSYNC),
-    .valid(VGA_BLANK_N),
-    .vga_r(VGA_R),
-    .vga_g(VGA_G),
-    .vga_b(VGA_B)
-);
+// vga_ctrl my_vga_ctrl(
+//     .pclk(clk),
+//     .reset(rst),
+//     .vga_data(vga_data),
+//     .h_addr(h_addr),
+//     .v_addr(v_addr),
+//     .hsync(VGA_HSYNC),
+//     .vsync(VGA_VSYNC),
+//     .valid(VGA_BLANK_N),
+//     .vga_r(VGA_R),
+//     .vga_g(VGA_G),
+//     .vga_b(VGA_B)
+// );
 
-ps2_keyboard my_keyboard(
-    .clk(clk),
-    .resetn(~rst),
-    .ps2_clk(ps2_clk),
-    .ps2_data(ps2_data)
-);
+// ps2_keyboard my_keyboard(
+//     .clk(clk),
+//     .resetn(~rst),
+//     .ps2_clk(ps2_clk),
+//     .ps2_data(ps2_data)
+// );
 
 seg mu_seg(
     .clk(clk),
@@ -69,26 +61,26 @@ seg mu_seg(
     .o_seg7(seg7)
 );
 
-vmem my_vmem(
-    .h_addr(h_addr),
-    .v_addr(v_addr[8:0]),
-    .vga_data(vga_data)
-);
+// vmem my_vmem(
+//     .h_addr(h_addr),
+//     .v_addr(v_addr[8:0]),
+//     .vga_data(vga_data)
+// );
 
 endmodule
 
-module vmem (
-    input [9:0] h_addr,
-    input [8:0] v_addr,
-    output [23:0] vga_data
-);
+// module vmem (
+//     input [9:0] h_addr,
+//     input [8:0] v_addr,
+//     output [23:0] vga_data
+// );
 
-reg [23:0] vga_mem [524287:0];
+// reg [23:0] vga_mem [524287:0];
 
-initial begin
-    $readmemh("resource/picture.hex", vga_mem);
-end
+// initial begin
+//     $readmemh("resource/picture.hex", vga_mem);
+// end
 
-assign vga_data = vga_mem[{h_addr, v_addr}];
+// assign vga_data = vga_mem[{h_addr, v_addr}];
 
-endmodule
+// endmodule
