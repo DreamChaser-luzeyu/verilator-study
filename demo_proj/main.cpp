14456464ff2426845072648649f37f66f0d150c5
DreamChaser <yydluzeyu@gmail.com>
2023-07-31 01:27:23 +0800

    Update main.cpp

--- demo_proj/main.cpp
+++ demo_proj/main.cpp
@@ -5,6 +5,7 @@
 #include "VTop.h"
 
 vluint64_t Sim_Time_GV = 0;
+VerilatedVcdC tfp;
 /**
  * Must implement, used by verilator 
  * Return current simulation time
@@ -17,13 +18,13 @@ int main(int argc, char **argv) {
     Verilated::commandArgs(argc, argv);
     // --- 用于导出vcd波形
     Verilated::traceEverOn(true);
-    VerilatedVcdC *tfp = new VerilatedVcdC;
+    // VerilatedVcdC *tfp = new VerilatedVcdC;
     // ---
     // 构造顶模块，此处的“top”将作为更上一层的模块的名称
     VTop *top = new VTop("top"); // 调用VTop.h里面的IO struct
     // --- 开始跟踪波形
-    top->trace(tfp, 0);
-    tfp->open("wave.vcd"); // 打开vcd
+    top->trace(&tfp, 0);
+    tfp.open("wave.vcd"); // 打开vcd
     // ---
     // 设置仿真时间段
     while (Sim_Time_GV < 20 && !Verilated::gotFinish()) {
@@ -48,12 +49,13 @@ int main(int argc, char **argv) {
                top->IO_Addend_3, 
                top->IO_Result); 
         // 存储波形文件
-        tfp->dump(Sim_Time_GV);
+        tfp.dump(Sim_Time_GV);
         Sim_Time_GV++;                                                                                                                // 推动仿真时间
     }
 
     top->final();
-    tfp->close();
+    tfp.flush();
+    tfp.close();
     delete top;
     return 0;
 }
